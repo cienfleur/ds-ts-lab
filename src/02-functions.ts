a3c768b6899e05d79b96a860437b2fe971e006be
cienfleur <redvelvet0214@protonmail.ch>
2025-09-26 11:54:53 +0100

    implement allOlder function

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,15 @@
 import {Friend, Colleague } from './myTypes'
+import { friends, colleagues } from './01-basics'
+
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(friends: Friend[]) : string[] {
+    friends.forEach( f => f.age += 1 )
+    return friends.map( f => `${f.name} is now ${f.age}` )
+}
+
+console.log(older(friends[0]))
+console.log(allOlder(friends))
