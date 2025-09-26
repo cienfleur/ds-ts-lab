aa26bf3f3d1fc0d61eaf60b03dd30339c79621cf
cienfleur <redvelvet0214@protonmail.ch>
2025-09-26 12:08:22 +0100

    Typed function signature

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -13,3 +13,30 @@ function allOlder(friends: Friend[]) : string[] {
 
 console.log(older(friends[0]))
 console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
+function addColleague(cs: Colleague[], name: string, department: string, email: string): Colleague {
+
+  const extension = highestExtension(cs).contact.extension + 1;
+  const colleague: Colleague = {
+    name: name,
+    department: department,
+    contact: {
+      email: email,
+      extension: extension
+    }
+  };
+  cs.push(colleague);
+  return colleague;
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
